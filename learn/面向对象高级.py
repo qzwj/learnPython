367576e799f64d357e19f55addf36343de35d540
liwenjie <502391211@qq.com>
2017-12-10 23:47:55 +0800

    枚举和type创建类

--- learn/面向对象高级.py
+++ learn/面向对象高级.py
@@ -224,3 +224,47 @@ class Chain1(object):
 			s += ':' + i
 		return self.__getattr__(s)
 print( Chain1().users('michael').repos )
+
+
+#枚举
+from enum import Enum, unique
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+#可以直接使用Month.Jan来引用一个常量，或者枚举它的所有成员
+print(Month.Jan)
+print(Month.Jan.value)
+for name, member in Month.__members__.items():
+	print(name, '=>', member, ',', member.value)
+
+
+#如果要更精准的控制枚举类型, 可以从Enum派生出自定义类
+
+@unique #这个装饰器可以检查保证没有重复值
+class Weekday(Enum):
+	Sun = 0
+	Mon = 1
+	Tue = 2
+	Wed = 3
+	Thu = 4
+	Fri = 5
+	Sat = 6
+
+print(Weekday.Mon)
+print(Weekday.Mon.name)
+print(Weekday['Tue'])
+print(Weekday.Tue.value)
+print(Weekday(2))
+
+
+#type 使用type来创建类
+def fn(self, name='world'): 
+	print('Hello, %s' % name)
+
+Hello = type('Hello', (object, ), dict(hello=fn)) #创建Hello class
+h = Hello()
+h.hello()
+
+# 1.class的名称；
+# 2.继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+# 3.class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+# 通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇到class定义时，仅仅是扫描一下class定义的语法，然后调用type()函数创建出class。
+
